28bfda53e9470598e1721e244c7caa145c3ca4bb
ozaki <fs65ugumori@yahoo.co.jp>
2018-11-09 07:57:26 +0900

    update server script

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 8888;
 const app = express();
+const port = process.env.PORT || 8888;
+const persons = [];
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -9,7 +13,17 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/persons', (req, res) => {
+  console.log(persons);
+  res.send(persons);
+});
+
+app.post('/persons', (req, res) => {
+  const person = req.body;
+  console.log(person);
+  persons.push(person);
+  return res.send(person);
+});
 
 app.listen(port, () => {
   console.log(`app start listening on port ${port}`);
