67b40b1d9ed5608cb39ff06168bcb1457d4056f3
ozaki <fs65ugumori@yahoo.co.jp>
2018-11-11 02:11:24 +0900

    divide fetch actions

---
+++ src/utils/fetchUtils.js
@@ -0,0 +1,62 @@
+import url from '../constants/url';
+
+export const getPersons = async () => {
+  try {
+    const res = await fetch(url.persons);
+    return res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const postPerson = async (person) => {
+  try {
+    const res = await fetch(url.persons, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(person),
+    });
+    return res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const getPerson = async (id) => {
+  try {
+    const res = await fetch(`${url.persons}/${id}`);
+    return res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const putPerson = async (id, person) => {
+  try {
+    const res = await fetch(`${url.persons}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(person),
+    });
+    return res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const deletePerson = async (id) => {
+  try {
+    const res = await fetch(`${url.persons}/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
